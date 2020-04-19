65c6c02c0e9bf445ccd17023f9089ad748e07863
hangjongnoh <topia2439@gmail.com>
2020-04-19 12:28:10 +0900

    moment 추가

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var moment = require('moment');
 const a =10;
-a = 1;
-b = 2;
+const b = 2;
+console.log(a+b);
+console.log(new Date() + 9);
+console.log( moment(new Date()).format('YYYY-MM-MM HH:mm:ss'));
