d7d5e7a5cb180452e027643155befb8687f498d4
hangjongnoh <nhj2163210@naver.com>
2020-04-26 13:46:20 +0900

    0425

--- 02.app.js
+++ 02.app.js
@@ -10,6 +10,11 @@ app.listen(3000, function() {
     console.log(path.join(__dirname, "./public"));
 });
 
+/* view 엔진 세팅 */
+app.set('View engine', 'pug'); //set은 변수 등록
+app.set('views', path.join(__dirname, './views'));
+app.locals.pretty = true;
+
 /*정적 Router */
 app.use(function(req, res, next) {
     sample = "SAMPLE";
@@ -22,17 +27,27 @@ app.use("/", express.static(path.join(__dirname, "./public"))); // 루트 폴더
 /* Router 구현 */
 app.get("/root", function(req, res, next) {
     res.send('<h1>Hello World' + sample + '</h1>');
-})
+});
 
 app.get("/query", function(req, res, next) {
     var name = req.query.name;
     res.send('<h1>Hello World' + name + '</h1>');
-})
+});
 
 app.get("/param/:name", function(req, res, next) {
     var name = req.params.name;
     res.send('<h1>Hello World' + name + '</h1>')
-})
+});
+app.get("/pug", (req, res, next) => {
+    const user = [
+        { id: 1, name: "홍길동" },
+        { id: 2, name: "홍길순" },
+        { id: 3, name: "홍길만" },
+    ];
+    const title = "pug로 만든 페이지 입니다.";
+    const headTitle = "pug 폐이지";
+    res.render("index.pug", { user, title, headTitle });
+});
 
 app.post("/board/save", function(req, res, next) {
     const { title, content } = req.body; //const title = req.body.title; const content = req.body.content; 와 같음
